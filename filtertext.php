44e5a84c724c66af1e5c1066a18db12b4762f75a
Thrillhause <johnson.kennethdean@gmail.com>
2019-05-01 21:15:23 -0700

    Filter text and shortcode

--- filtertext.php
+++ filtertext.php
@@ -9,3 +9,17 @@
  * Author URI: https://github.com/thrillhause
  * 
  */
+
+ add_filter('the_content', 'contentEdits');
+
+ function contentEdits($content){
+   $content = $content . '<p>Testing Content Filter</p>';
+   $content = str_replace('Lorem','@#&@!*', $content);
+   return $content;
+ }
+
+ add_shortcode('filterText', 'shortCodeTest');
+
+ function shortCodeTest(){
+   return 'Filtered ShortCode';
+ }
