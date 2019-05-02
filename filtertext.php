8719aeb1f7c753aff13cc11c685b0a54f3fb3434
Thrillhause <johnson.kennethdean@gmail.com>
2019-05-01 20:58:30 -0700

    Init commit

---
+++ filtertext.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * 
+ * Plugin Name: Filter Text Plugin
+ * Description: Filter out specific words on your pages and posts.
+ * Version: 0.1
+ * Author: Dean Johnson
+ * Author URI: https://github.com/thrillhause
+ * 
+ */
